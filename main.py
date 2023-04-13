105c914bb160841adef0e5322df2230bd5fe6da1
maticstric <matic@likar.me>
2023-04-13 11:02:33 -0700

    Get from API and print trip updates

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+def parse_feed(api_key, format):
+    url = f'https://api.goswift.ly/real-time/vta/gtfs-rt-trip-updates?apiKey={api_key}&format={format}'
+
+    response = requests.get(url)
+    data = json.loads(response.text)
+    trip_updates = data['entity']
+
+    for trip_update in trip_updates:
+        print(trip_update)
+
+if __name__ == '__main__':
+    parse_feed('59af72683221a1734f637eae7a7e8d9b', 'json')
