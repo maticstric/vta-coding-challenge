40892bc586a3a26642178a14d91fdecc3abbc1f8
maticstric <matic@likar.me>
2023-04-13 11:19:54 -0700

    Use sqlalchemy to make sqlite db

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+from sqlalchemy import create_engine
+
 def parse_feed(api_key, format):
     url = f'https://api.goswift.ly/real-time/vta/gtfs-rt-trip-updates?apiKey={api_key}&format={format}'
 
@@ -13,3 +15,7 @@ def parse_feed(api_key, format):
 
 if __name__ == '__main__':
     parse_feed('59af72683221a1734f637eae7a7e8d9b', 'json')
+
+    engine = create_engine('sqlite:///gtfs.sqlite', echo=True)
+
+    conn = engine.connect()
