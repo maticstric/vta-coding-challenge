b738690a7682a367b078350fba9d7c25ba657797
maticstric <matic@likar.me>
2023-04-13 15:11:18 -0700

    Basic inserting into database, with foreign keys/one-to-many relationship implemented

--- main.py
+++ main.py
@@ -2,6 +2,41 @@ import requests
 import json
 
 from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+Base = declarative_base()
+
+class TripUpdate(Base):
+    __tablename__ = 'trip_updates'
+
+    id = Column(String, primary_key=True)
+
+    trip_id = Column(String)
+    start_time = Column(String)
+    start_date = Column(String)
+    schedule_relationship = Column(String)
+    route_id = Column(String)
+    direction_id = Column(Integer)
+
+    stop_time_updates = relationship('StopTimeUpdate', backref='trip_update')
+
+class StopTimeUpdate(Base):
+    __tablename__ = 'stop_time_updates'
+
+    # Custom unique id which is the TripUpdate id concatenated to
+    # the stopSequence (with an underscore in between). Something like this
+    # is necessary since no unique id is provided in the API.
+    id = Column(String, primary_key=True)
+
+    stop_id = Column(String)
+    stop_sequence = Column(Integer)
+    arrival_time = Column(String)
+    departure_time = Column(String)
+    schedule_relationship = Column(String)
+
+    trip_update_id = Column(String, ForeignKey('trip_updates.id'))
 
 def parse_feed(api_key, format):
     url = f'https://api.goswift.ly/real-time/vta/gtfs-rt-trip-updates?apiKey={api_key}&format={format}'
@@ -11,11 +46,47 @@ def parse_feed(api_key, format):
     trip_updates = data['entity']
 
     for trip_update in trip_updates:
-        print(trip_update)
+        trip = trip_update['tripUpdate']['trip']
+
+        trip_update_entity = TripUpdate(
+            id = trip_update['id'],
+            trip_id = trip['tripId'],
+            start_time = trip['startTime'],
+            start_date = trip['startDate'],
+            schedule_relationship = trip['scheduleRelationship'],
+            route_id = trip['routeId'],
+            direction_id = trip['directionId']
+        )
+
+        # Some trip updates don't have a stopTimeUpdate
+        if 'stopTimeUpdate' not in trip_update['tripUpdate']: continue
+
+        for stop_time_update in trip_update['tripUpdate']['stopTimeUpdate']:
+            stop_time_update_entity = StopTimeUpdate(
+                id = f"{trip_update_entity.id}_{stop_time_update['stopSequence']}",
+                stop_id = stop_time_update['stopId'],
+                stop_sequence = stop_time_update['stopSequence'],
+                schedule_relationship = stop_time_update['scheduleRelationship'],
+                trip_update_id = trip_update_entity.id
+            )
+
+            if 'arrival' in stop_time_update:
+                stop_time_update_entity.arrival_time = stop_time_update['arrival']['time']
+
+            if 'departure' in stop_time_update:
+                stop_time_update_entity.departure_time = stop_time_update['departure']['time']
+
+            session.add(stop_time_update_entity)
+
+        session.add(trip_update_entity)
+
+    session.commit()
 
 if __name__ == '__main__':
-    parse_feed('59af72683221a1734f637eae7a7e8d9b', 'json')
-
     engine = create_engine('sqlite:///gtfs.sqlite', echo=True)
+    Base.metadata.create_all(engine)
 
-    conn = engine.connect()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    parse_feed('59af72683221a1734f637eae7a7e8d9b', 'json')
