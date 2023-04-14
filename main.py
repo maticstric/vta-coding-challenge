0eb031103befbae7497c6c24d52112126522daed
maticstric <matic@likar.me>
2023-04-13 18:21:49 -0700

    Implement 'Remove expired TripUpdates every time the command is executed.'

--- main.py
+++ main.py
@@ -63,9 +63,22 @@ def add_stop_time_update_entity(stop_time_update_entity, session):
     if not exists:
         session.add(stop_time_update_entity)
 
-def parse_feed(api_key, format, session):
-    data = get_json_data(api_key, format)
+def delete_expired_records(data, session):
+    records = session.query(TripUpdate).all()
 
+    for record in records:
+        record_expired = True
+
+        for trip_update in data['entity']:
+            if trip_update['id'] == record.id:
+                record_expired = False
+
+        if record_expired:
+            session.query(TripUpdate).filter(TripUpdate.id==record.id).delete()
+
+    session.commit()
+
+def parse_feed(data, session):
     trip_updates = data['entity']
 
     for trip_update in trip_updates:
@@ -81,28 +94,27 @@ def parse_feed(api_key, format, session):
             direction_id = trip['directionId']
         )
 
-        # stopTimeUpdate is optional so we need to check if it exists
-        if 'stopTimeUpdate' not in trip_update['tripUpdate']: continue
+        # stopTimeUpdate is optional (for CANCELED) so we need to check if it exists
+        if 'stopTimeUpdate' in trip_update['tripUpdate']:
+            for stop_time_update in trip_update['tripUpdate']['stopTimeUpdate']:
+                stop_time_update_entity = StopTimeUpdate(
+                    id = f"{trip_update_entity.id}_{stop_time_update['stopSequence']}",
+                    stop_id = stop_time_update['stopId'],
+                    stop_sequence = stop_time_update['stopSequence'],
+                    schedule_relationship = stop_time_update['scheduleRelationship'],
+                    trip_update_id = trip_update_entity.id
+                )
 
-        for stop_time_update in trip_update['tripUpdate']['stopTimeUpdate']:
-            stop_time_update_entity = StopTimeUpdate(
-                id = f"{trip_update_entity.id}_{stop_time_update['stopSequence']}",
-                stop_id = stop_time_update['stopId'],
-                stop_sequence = stop_time_update['stopSequence'],
-                schedule_relationship = stop_time_update['scheduleRelationship'],
-                trip_update_id = trip_update_entity.id
-            )
+                # The arrival and departure times are optional so we need to check if they exist
 
-            # The arrival and departure times are optional so we need to check if they exist
+                if 'arrival' in stop_time_update:
+                    stop_time_update_entity.arrival_time = stop_time_update['arrival']['time']
 
-            if 'arrival' in stop_time_update:
-                stop_time_update_entity.arrival_time = stop_time_update['arrival']['time']
+                if 'departure' in stop_time_update:
+                    stop_time_update_entity.departure_time = stop_time_update['departure']['time']
 
-            if 'departure' in stop_time_update:
-                stop_time_update_entity.departure_time = stop_time_update['departure']['time']
-
-            
-            add_stop_time_update_entity(stop_time_update_entity, session)
+                
+                add_stop_time_update_entity(stop_time_update_entity, session)
 
         add_trip_update_entity(trip_update_entity, session)
 
@@ -115,4 +127,7 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    parse_feed('59af72683221a1734f637eae7a7e8d9b', 'json', session)
+    data = get_json_data('59af72683221a1734f637eae7a7e8d9b', 'json')
+
+    delete_expired_records(data, session)
+    parse_feed(data, session)
